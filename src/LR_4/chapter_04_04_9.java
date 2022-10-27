acde68e73fcc7863088d2e0932292684c13042c5
SanguiniAvari <volk-post@yandex.ru>
2022-10-27 20:59:43 +0500

    LR 4

--- src/LR_4/chapter_04_04_9.java
+++ src/LR_4/chapter_04_04_9.java
@@ -22,27 +22,39 @@ public class chapter_04_04_9 {
         char[] ArrayChar = encryptString.toCharArray();
 
         // преобразуем в крокозябру
-
+        // создаем новый массив
         char[] MutArray = new char[ArrayChar.length];
+        /* вводим переменную в которую в последствии будем помещать индекс найденного символа
+         и прибавлять к нему смещение */
         int kir = 0;
-
+        //создаем первый цикл, который проходит по введенной строке посимвольно
         for (int i=0; i<ArrayChar.length; i++){
+            //помещаем в временную переменную значение символа введенной строки
             char temp = ArrayChar[i];
+            // создаем второй цикл для поиска переменной в созданном словаре
             for (int q=0; q < arr_char.length; q++){
                 if(temp == arr_char[q]){
+                    // когда символ найден помещаем ешго в переменную и прибавляем значение смещения
                     kir = q+shift;
+                    // проверяем не выходит ли полученный индекс за пределы массива в большую сторону
                     if (kir >= arr_char.length){
+                        // если выходит то запускаем цикл по уменьшению значения
                         while (kir >= arr_char.length){
                             kir = kir - arr_char.length;
                         }
+                        // помещаем полученный символ крокозябры в массив символов кровозябры
                         MutArray[i] = arr_char[kir];
+                        // прерываем цикл, так как уже нашли что искали
                         break;
+                    /* проверяем не выходит ли полученный индекс за пределы массива в меньшую сторону,
+                    аналогично как в большую */
                     } else if (kir < 0){
                         while (kir < 0){
                             kir = kir + arr_char.length;
                         }
                         MutArray[i] = arr_char[kir];
                         break;
+                        // если всё тип-топ, то просто записываем кроко-символ в массив
                     } else{
                         MutArray[i] = arr_char[kir];
                         break;
@@ -51,8 +63,9 @@ public class chapter_04_04_9 {
             }
 
         }
-
+        // преобразуем полученную крокозябру в строку
         String MutString = new String(MutArray);
+        // возвращаем строку
         return MutString;
     }
 
@@ -64,16 +77,20 @@ public class chapter_04_04_9 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите текст для шифрования: ");
         String line = scanner.nextLine();
+        // Запрашиваем ключ
         System.out.print("Введите ключ: ");
         int key = scanner.nextInt();
+        // Запускаем метод преобразования и выводим в консоль
         String reline = getArray(line,key);
         System.out.print("\nТекст после преобразования: ");
         System.out.print(reline);
 
+        // спрашиваем об обратном преобразовании
         while (true) {
             System.out.print("\nВыполнить обратное преобразование? (y/n): ");
             char q = scanner.next().charAt(0);
             if (q == 'y') {
+            // если получили ответ, что обратно нужно преобразовать, то запускаем обратный процесс изменяя знак ключа
                 String unpass = getArray(reline, -key);
                 System.out.print("Был зашифрован текст: ");
                 System.out.println(unpass);
